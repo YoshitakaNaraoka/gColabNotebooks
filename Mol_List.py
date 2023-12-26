291f1fdb0cc912c6962e5e329146106a6fc8d44b
Placeless <mikoprpr@hotmail.com>
2023-12-26 19:13:28 +0900

    no message

--- Mol_List.py
+++ Mol_List.py
@@ -5,4 +5,6 @@ alanine = Chem.MolFromSmiles('CC(C(=O)O)N')
 phenylalanine = Chem.MolFromSmiles('c1ccccc1CC(C(=O)O)N')
 crown = Chem.MolFromSmiles('C1COCCOCCOCCOCCOCCO1')
 ciguatoxin = Chem.MolFromSmiles('CC1CC2C(CC3C(O2)C(C(C4C(O3)C(C(C5(O4)CC(CO5)O)C)C)O)C)OC6CC7C(C(CC8C(O7)CC=CCC9C(O8)C=CC2C(O9)C=CC3C(O2)CC2C(O3)C(C3C(O2)CC=CC(O3)C=CC(CO)O)O)O)(OC6C1)C')
-mol_list = benzene,alanine,phenylalanine,crown,ciguatoxin
+mol_list = [
+  benzene,alanine,phenylalanine,crown,ciguatoxin
+  ]
